91faa024220890bc08bf573e9ac841562019ffed
admin <admin@admin.com>
2022-02-04 12:50:30 +0300

    feature: add CRUD API for Category and Expense

--- backend/fc_root/expenses/models.py
+++ backend/fc_root/expenses/models.py
@@ -6,9 +6,12 @@ class Category(models.Model):
     codename = models.CharField(max_length=255, primary_key=True, unique=True)
     is_regular_expense = models.BooleanField()
 
+    def __str__(self):
+        return f'{self.codename} ({self.name})'
+
 
 class Expense(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True) # AutoField
     amount = models.FloatField()
     created = models.DateTimeField(auto_now_add=True)
     category_codename = models.ForeignKey(Category, on_delete=models.CASCADE)
--- backend/fc_root/fc/urls.py
+++ backend/fc_root/fc/urls.py
@@ -1,24 +1,8 @@
-"""fc URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from expenses.views import RESTCategoryList, RESTCategoryDetails
+from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("rest-api/categories/", RESTCategoryList.as_view(), name="rest_category_list"),
-    path("rest-api/categories/<str:codename>/", RESTCategoryDetails.as_view(), name="rest_category_detail")
+    path('rest-api/', include('expenses.urls'))
 ]
